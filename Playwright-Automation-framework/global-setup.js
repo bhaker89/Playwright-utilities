6e05269ce0b7e51b3fbca0374681616e8cf45437
varun bhaker <varun.bhaker@1mg.com>
2026-03-26 12:36:59 +0530

    chore: remove duplicate framework folder

--- Playwright-Automation-framework/global-setup.js
+++
@@ -1,9 +0,0 @@
-const { CleanupHelper } = require('./utils/base/cleanup-helper');
-
-async function globalSetup(config) {
-    // Prune allure-results if they exceed 500 files
-    // (Allure creates many small files per test)
-    CleanupHelper.pruneDirectory('allure-results', 500);
-}
-
-module.exports = globalSetup;
